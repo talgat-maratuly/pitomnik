ed44d6deec0f3a3ee89a255defb2ee8b5d927525
talgat-maratuly <h526kz5pzn@privaterelay.appleid.com>
2026-06-26 12:18:39 +0500

    feat: add public access check to RolesGuard for improved authorization handling

--- apps/api/src/common/guards/roles.guard.ts
+++ apps/api/src/common/guards/roles.guard.ts
@@ -1,5 +1,6 @@
 import { CanActivate, ExecutionContext, ForbiddenException, Injectable } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
+import { IS_PUBLIC_KEY } from '../decorators/public.decorator';
 import { ROLES_KEY } from '../decorators/roles.decorator';
 import { UserRole } from '../enums/user-role.enum';
 import { User } from '../../entities/user.entity';
@@ -9,6 +10,12 @@ export class RolesGuard implements CanActivate {
   constructor(private readonly reflector: Reflector) {}
 
   canActivate(context: ExecutionContext): boolean {
+    const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+    if (isPublic) return true;
+
     const requiredRoles = this.reflector.getAllAndOverride<UserRole[]>(ROLES_KEY, [
       context.getHandler(),
       context.getClass(),
